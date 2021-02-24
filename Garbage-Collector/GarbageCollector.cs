27c5a0e38b611273c199a6f602519830ebf14fd5
CoelWu <coelwu78@protonmail.com>
2021-02-24 18:47:32 +0800

    [Initial Upload] Upload Source Code

---
+++ Garbage-Collector/GarbageCollector.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Garbage_Collector
+{
+    public class GarbageCollector
+    {
+		[DllImport("kernel32.dll")]
+		private static extern bool SetProcessWorkingSetSize(IntPtr proc, int min, int max);
+
+		private void FlushMemory()
+		{
+			GC.Collect();
+			GC.WaitForPendingFinalizers();
+			if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+			{
+				SetProcessWorkingSetSize(Process.GetCurrentProcess().Handle, -1, -1);
+			}
+		}
+
+		public void StartCollecting(int sleepSpan = 30)
+		{
+			Task.Factory.StartNew(delegate ()
+			{
+				for (;;)
+				{
+					try
+					{
+						FlushMemory();
+						Thread.Sleep(TimeSpan.FromSeconds((double)sleepSpan));
+					}
+					catch (Exception)
+					{
+					}
+				}
+			});
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# Garbage-Collector
-C# WPF Hack for reducing memory usage
+<p align="center">
+    <img src="https://i.loli.net/2021/02/24/EzIOcftFVg6huXQ.png" align="center" height="80"/>
+</p>
+
+<div align="center">
+
+# 垃圾回收 .NET SDK
+
+[![Nuget](https://img.shields.io/nuget/v/CoelWu.Garbage.Collector)](https://www.nuget.org/packages/CoelWu.Garbage.Collector/)
+
+该SDK在.NET Standard 2.0上构建
+
+</div>
+
+## 简单的开始
+
+使用前，你需要先安装 **CoelWu.Garbage.Collector** nuget包
+
+```csharp
+// 创建实例
+public GarbageCollector garbageCollector = new GarbageCollector();
+
+protected override void OnStartup(StartupEventArgs e)
+{
+    // 开始收集清理 (默认30s执行一次)
+    garbageCollector.StartCollecting();
+    base.OnStartup(e);
+}
+```
+
+## 图标版权
+
+图标来自于Nhor Phai.
+
+- [Nhor Phai](https://www.flaticon.com/authors/nhor-phai)
